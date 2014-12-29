d528aa3a4ebed15422b0673146155911d4150cb1
Breland Miley <brelandm@amazon.com>
2014-12-29 01:38:07 -0800

    Adds getter setter for fields in Yeild

--- src/com/simplerecipemanager/core/Yield.java
+++ src/com/simplerecipemanager/core/Yield.java
@@ -2,4 +2,16 @@ package com.simplerecipemanager.core;
 
 public class Yield {
 	private long serves;
+
+	public Yield() {
+
+	}
+
+	public long getServes() {
+		return serves;
+	}
+
+	public void setServes(long serves) {
+		this.serves = serves;
+	}
 }
